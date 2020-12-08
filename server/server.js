73fcb39db4fd161e34e62acdb0bb9c4b269a7768
Alan.Rivera <rivera.alan.3m@gmail.com>
2020-12-07 21:31:56 -0600

    CORS agregados

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
 // Habilitar CORS
-app.use(cors());
+// app.use(cors());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
